3efd445ed3f160a71bfb9b968300ecd71ab85362
DUT-SUN <SUN030920@163.com>
2022-04-17 16:29:52 +0800

    Java HelloWorld

---
+++ 20220417/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.lang.management.PlatformLoggingMXBean;
+
+/**
+ * @Author 12629
+ * @Date 2022/3/9 10:27
+ * @Descirption:begin
+ */
+public class HelloWorld {
+    public static void main(String[]args)
+    {
+        System.out.println("hello world!");
+    }
+
+}
